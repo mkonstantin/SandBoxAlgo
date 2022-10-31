7261e18c588e4d1b72f166e090ec0148eaba72d9
mkonstantin <konstantin.malyshev@indriver.com>
2022-10-31 08:29:45 +0600

    урок 2

--- panic_lesson/panic.go
+++ panic_lesson/panic.go
@@ -5,10 +5,11 @@ import "fmt"
 type PanicLesson struct{}
 
 func (p *PanicLesson) Start() {
-	lessonOne()
+	//lessonOne()
+	lessonTwo()
 }
 
-// Primer
+// Primer 1
 // функции defer всегда вызываются после паники
 // результат будет:
 // "go  now step 1" -> "defer function run"
@@ -17,6 +18,26 @@ func lessonOne() {
 		fmt.Println("defer function run")
 	}()
 
-	fmt.Println("go  now step 1")
+	fmt.Println("go now step 1-1")
+	panic("we got a panic")
+}
+
+// Урок 2 Паникуем и восстанавливаемся
+// Вызываем панику, в Дефер функции вызываем recover()
+// И дальше продолжаем работу пропуская функцию с паникой
+func lessonTwo() {
+	fmt.Println("Шаг 1: вызываем функцию с паникой")
+	PanicFunc()
+	fmt.Println("Шаг 4: восстановились после паники как ни в чем не бывало")
+}
+
+func PanicFunc() {
+	defer func() {
+		fmt.Println("defer function run")
+		fmt.Println("Шаг 3: Начинаем восстанавливаться")
+		recover()
+	}()
+
+	fmt.Println("Шаг 2: Паникуем")
 	panic("we got a panic")
 }
