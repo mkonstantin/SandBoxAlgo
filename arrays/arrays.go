2c8bd86d72163a7dfd1b2f07c7e252573fe3dde1
mkonstantin <konstantin.malyshev@indriver.com>
2022-11-02 09:42:36 +0600

    фвв

--- arrays/arrays.go
+++ arrays/arrays.go
@@ -34,12 +34,28 @@ func lessonFour() {
 }
 
 func lessonFive() {
-	var mymap = map[string]string{} // - инициализировал
-	//mymap := make(map[string]string) // - инициализировал
-	//var mymap map[string]string // - просто объявил переменную
-
+	//var mymap = map[string]string{} // - инициализировал 1 способ
+	//mymap := make(map[string]string) // - инициализировал 2 способ
+	//var mymap map[string]string     // - объявил переменную и создал с помощью make 3 способ
+	//mymap = make(map[string]string) // |
+	mymap := map[string]string{"a1": "A1", "a2": "A2", "a3": "A3"}
 	mymap["asd"] = "fsg"
+
 	for key, value := range mymap {
 		fmt.Printf("\nkey: %s value: %s", key, value)
 	}
+}
+
+// Таким образом можно проверять есть ли значение в переменной карты или его нет
+// при обращении к ключу карты ok покажет, есть такой ключ или его нет
+func lesson6() {
+	mymap := make(map[string]string)
+	mymap["a1"] = "A1"
+	mymap["a2"] = "A2"
+
+	value, ok := mymap["a1"] // тут есть
+	fmt.Printf("\nvalue: %s isExist: %v", value, ok)
+
+	value, ok = mymap["DDD"] // такого ключа нет
+	fmt.Printf("\nvalue: %s isExist: %v", value, ok)
 }
